6237db1a78e452841688936679806d15a5b48d4a
maheep05 <maheepsingh458@gmail.com>
2024-02-12 10:22:42 +0530

    hello

---
+++ src/pages/List.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+import { Navbar } from "../components/NavBar";
+
+export function List() {
+    return (
+        <div>
+            <Navbar />
+            <Header type="list"/>
+
+        </div>
+    )
+}
